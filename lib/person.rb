a9c812a18418c2e1fed8f0ca6c5576b01138f081
CleanSolLLC <mamark90@gmail.com>
2019-09-26 22:55:45 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Person
   
   	attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
@@ -5,7 +6,7 @@ class Person
 				:wrist_size, :glove_size, :pant_length, :pant_width
 
 	def initialize(data)
-
+		binding.pry
 	data.each do |key, value| 
 		self.send(("#{key}="), value)
 		end
